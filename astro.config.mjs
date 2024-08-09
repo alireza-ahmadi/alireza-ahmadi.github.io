9302caaa0f80a0c0127bd7d6e178806b86346e33
Alireza Ahmadi <a.ahmadi94@gmail.com>
2024-08-09 17:34:58 +0330

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://alireza.es',
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
 });
